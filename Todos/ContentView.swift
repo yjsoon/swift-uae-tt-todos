a417ab4bc39e161f81140612a2b6b1f0bde3bffc
YJ Soon <yjsoon@tinkertanker.com>
2023-02-24 14:58:25 +0800

    ContentView now goes to TodoDetailView (but doesn't pass in any data yet)

--- Todos/ContentView.swift
+++ Todos/ContentView.swift
@@ -18,20 +18,29 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             List($todos) { $todo in
-                HStack {
-                    Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle")
-                        .onTapGesture {
-                            todo.isCompleted.toggle()
-                        }
-                    VStack(alignment: .leading) {
-                        Text(todo.title)
-                            .strikethrough(todo.isCompleted)
-                        if !todo.subtitle.isEmpty {
-                            Text(todo.subtitle)
-                                .font(.subheadline)
-                                .foregroundColor(.gray)
+                
+                NavigationLink {
+                    
+                    TodoDetailView()
+                    
+                } label: {
+                    
+                    HStack {
+                        Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle")
+                            .onTapGesture {
+                                todo.isCompleted.toggle()
+                            }
+                        VStack(alignment: .leading) {
+                            Text(todo.title)
+                                .strikethrough(todo.isCompleted)
+                            if !todo.subtitle.isEmpty {
+                                Text(todo.subtitle)
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
                         }
                     }
+                    
                 }
             }
             .navigationTitle("Todos")
