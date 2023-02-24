ebc9877183eb99ca7e818f56866d53f519aa618e
YJ Soon <yjsoon@tinkertanker.com>
2023-02-24 14:13:11 +0800

    Initial Commit

---
+++ Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by YJ Soon on 24/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
