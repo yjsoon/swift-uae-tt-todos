36cbc074526bba35aa5fb8e3cf7a4a3cbb98e4dd
YJ Soon <yjsoon@tinkertanker.com>
2023-02-24 14:32:06 +0800

    Added NavigationStack

--- Todos/ContentView.swift
+++ Todos/ContentView.swift
@@ -16,8 +16,11 @@ struct ContentView: View {
     ]
     
     var body: some View {
-        List(todos) { todo in
-            Text(todo.title)
+        NavigationStack {
+            List(todos) { todo in
+                Text(todo.title)
+            }
+            .navigationTitle("Todos")
         }
     }
 }
